8d9ee8eeeb56edfb8c246c8da6c28b0b08238834
Arthur Kovalchuk <moysha.tsoref@gmail.com>
2022-08-28 14:16:41 +0300

    new 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,61 @@
+import * as os from 'os';
 import * as readPkgUp from 'read-pkg-up';
+import * as requestIp from 'request-ip';
+
+const ifaces = os.networkInterfaces();
+const destinationAddress: string = ifaces.en0.find(_item => _item.family === 'IPv4' && _item.internal === false).address;
+
+/**
+ * Уведомление микросервиса безопасности о пользовательском действии
+ */
+
 const configPromise = readPkgUp.sync();
+export async function notifyUserAction (arg: {
+    securityClient: any,
+    token: string,
+    action: string,
+    data: any,
+    request: any,
+    severity: number,
+    eventOutcome: boolean,
+    message: string,
+    endTime: number,
+    filename?: string
+}): Promise<void> {
 
-
-export async function testActionLib() {
     const conf = await configPromise;
-    console.log(conf);
+    const deviceVendor: string | undefined = conf?.custom?.['device-vendor'];
+    if (!deviceVendor) {
+        throw new Error('Please add property "custom"."device-vendor" in package.json');
+    }
+    const deviceProduct: string | undefined = conf?.name;
+    if (!deviceProduct) {
+        throw new Error('Please add property "name" in package.json');
+    }
+    const deviceVersion: string | undefined = conf?.version;
+    if (!deviceVersion) {
+        throw new Error('Please add property "version" in package.json');
+    }
+
+    const sourceAddress = requestIp.getClientIp(arg.request);
+    const sourceHostName = arg.request.headers.host;
+
+    return await arg.securityClient
+        .send({ cmd: 'CREATE_USER_ACTION' }, {
+            token: arg.token,
+            action: arg.action,
+            data: arg.data,
+            deviceVendor,
+            deviceProduct,
+            deviceVersion,
+            sourceAddress,
+            destinationAddress,
+            sourceHostName,
+            eventOutcome: arg.eventOutcome === true ? 'success' : 'failure',
+            message: arg.message,
+            endTime: arg.endTime,
+            filename: arg?.filename,
+            severity: arg.severity,
+        })
+        .toPromise();
 }
